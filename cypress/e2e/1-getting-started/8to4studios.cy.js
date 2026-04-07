17fc31bf42c7ce31fffbfc330897f9dd48bdf19a
Dan Allen <dsallen7@gmail.com>
2026-04-07 11:34:16 -0400

    Check news article exists

--- cypress/e2e/1-getting-started/8to4studios.cy.js
+++ cypress/e2e/1-getting-started/8to4studios.cy.js
@@ -3,17 +3,21 @@
 describe('Checks 8to4studios navbar', () =>{
     beforeEach( () => {
         cy.visit('https://8to4studios.com/')
-    })
+    });
 
     it('Check navbar items displayed', () => {
         cy.contains('News').should('have.text', 'News');
         cy.contains('Forum').should('have.text', 'Forum');
         cy.contains('Login').should('have.text', 'Login');
         cy.contains('Register').should('have.text', 'Register');
-    })
+    });
+
+    it('Check news article exists', () => {
+        cy.contains('This is a title').should('have.text', 'This is a title');
+    });
 
     it('Check forum post exists', () => {
         cy.get('#mainmenuforumbtn').click();
         cy.contains('Find Me').should('have.text', 'Find Me');
-    })
+    });
 });
