86d3b3db27f3d1d7150eaa7086aa26751555399a
Dan Allen <dsallen7@gmail.com>
2026-04-07 11:39:59 -0400

    Remove google captcha

--- cypress/e2e/1-getting-started/google.cy.js
+++ cypress/e2e/1-getting-started/google.cy.js
@@ -12,8 +12,5 @@ describe('Go to basic webpage', () => {
   it('Query Google', () =>{
     cy.get('.gLFyf').type('Youtube');
     cy.contains('Google Search').click();
-    cy.frameLoaded('iframe[title="reCAPTCHA"]');
-    cy.iframe('iframe[title="reCAPTCHA"]').find('.recaptcha-checkbox-border').should('be.visible').click();
-    cy.frameLoaded('iframe[title="recaptcha challenge expires in two minutes"]');
   })
 })
